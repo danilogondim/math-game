5da6515b2bab71f64eb9119c9574a0649fdb53c9
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-12 18:51:01 +0000

    Add logic to generate and answer questions

--- game.rb
+++ game.rb
@@ -2,26 +2,21 @@ class Game
   def initialize
     @player_1 = Player.new("Player 1")
     @player_2 = Player.new("Player 2")
-    @players = []
+    @players = [@player_1, @player_2]
     @turn = 1
-    @current_player = ""
-
   end
 
   def setup
     @player_1.get_name
     @player_2.get_name
     puts "Alright, #{@player_1.name} and #{@player_2.name}! Let's do some math!!"
-    @players << @player_1
-    @players << @player_2
     @current_player = @players.rotate!(rand(2)).first
-    p @current_player.name
   end
 
   def new_turn
     @current_player = @players.rotate!.first
-    puts "----- TURN #{@turn} -----"
-
+    puts "--------------- TURN #{@turn} ---------------"
+    @current_question = Question.new
   end
 
   def score
@@ -30,19 +25,21 @@ class Game
 
   def game_over
     @current_player = @players.last
+    puts "------------- GAME OVER --------------"
     puts "#{@current_player.name} wins with a score of #{@current_player.lives}/3"
-    puts "----- GAME OVER -----"
     puts "Good bye!"
   end
 
   def play
     setup
-
-
     until @player_1.is_dead? || @player_2.is_dead?
       new_turn
-      p @current_player.name
-      @current_player.lose_life
+      puts "#{@current_player.name}: #{@current_question.generate_question}"
+      print ">"
+      answer = gets.chomp.to_i
+      if !@current_question.is_right?(answer)
+        @current_player.lose_life
+      end
       score
     end
     
